87105d7c3eb6b302b49bf9c33b099467844fe6aa
EDMUNDO RAMIREZ JIMENEZ <edmundoramirezu7876@outlook.com>
2026-07-06 02:23:59 -0600

    Add components module exports

---
+++ src/components/mod.rs
@@ -0,0 +1,15 @@
+pub mod header;
+pub mod hero;
+pub mod about;
+pub mod skills;
+pub mod projects;
+pub mod contact;
+pub mod footer;
+
+pub use header::Header;
+pub use hero::Hero;
+pub use about::About;
+pub use skills::Skills;
+pub use projects::Projects;
+pub use contact::Contact;
+pub use footer::Footer;
